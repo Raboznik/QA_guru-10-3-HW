fc4982405d21c5e329e5e01e7fe8efad7ffc5a58
Максим <Raboznik@yandex.ru>
2021-12-24 23:16:19 +0300

    fix wiki-pages-box checking

--- src/test/java/github/SoftAssertions.java
+++ src/test/java/github/SoftAssertions.java
@@ -4,13 +4,13 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
 
 
 /**
  * Есть ли разница между $("h1 div"); и $("h1").$("div");
- *
+ * <p>
  * Есть.
  * Если в DOM есть несколько элементов h1, а в первом h1 нету <div></div>,
  * то $("h1 div") посмотрит не только первый элемент h1.
@@ -23,8 +23,11 @@ public class SoftAssertions {
 
         Selenide.open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $("#wiki-body .markdown-body").shouldHave(text("Soft assertions"));
-        $x("//*[@id=\"wiki-body\"]/div/ul[1]/li[6]/a").click();
+        $(".js-wiki-more-pages-link").click();
+
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
 }
